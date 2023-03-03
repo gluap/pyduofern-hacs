05f455d5b34ebe67c26889f929c8c91671785a90
Tilman Adler <tilman-git@t-animal.de>
2023-03-03 21:32:15 +0100

    Add button for toggling shutter movement
    Home-Assistant doesn't remember the last movement direction and
    just toggles between "not closed"/"closed". The duofern-internal
    toggle behaves way better: when moving the shutter stops, when
    stopped it moves in the opposite direction of the last movement.
    This new entity calls this duofern-enabled toggle mechanism.
    Since it doesn't have state, a button is the appropriate entity
    type, even though there is no corresponding button on the actual
    device.

---
+++ custom_components/duofern/button.py
@@ -0,0 +1,69 @@
+import logging
+
+from typing import List
+
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+
+from homeassistant.components.button import ButtonEntity
+
+from custom_components.duofern.cover import DuofernShutter, is_shutter
+from pyduofern.duofern_stick import DuofernStickThreaded
+
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+def setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None) -> None:
+    """Setup the Duofern cover platform."""
+
+    stick = hass.data[DOMAIN]['stick']
+    alreadyAddedEntityIds = hass.data[DOMAIN]['devices'].keys()
+
+    to_add: List[ButtonEntity] = []
+    _LOGGER.info("found: " + str(stick.config['devices']))
+    _LOGGER.info("already added: " + str(stick.config['devices']))
+    for duofernDevice in stick.config['devices']:
+        duofernId: str = duofernDevice['id']
+        if not is_shutter(duofernId) or duofernId not in alreadyAddedEntityIds:
+            _LOGGER.info("skipping: " + str(duofernId))
+            continue
+
+        entityId = duofernId + "_toggle"
+        if entityId not in alreadyAddedEntityIds:        
+            to_add.append(DuofernShutterToggleButton(duofernId, stick))
+
+    add_entities(to_add)
+
+class DuofernShutterToggleButton(ButtonEntity):
+    def __init__(self, duofernId: str, stick: DuofernStickThreaded):
+        """Initialize the toggle button for the shutter."""
+        self._duofernId = duofernId
+        self._stick = stick
+
+    @property
+    def name(self) -> str:
+        return self._duofernId + " Toggle"
+
+    @property
+    def unique_id(self) -> str:
+        return self._duofernId + "_toggle"
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        """Return information about the device the entity belongs to group entities to devices"""
+        return {
+            "identifiers": { 
+                (DOMAIN, self._duofernId)
+            }
+        } #type: ignore #(We only care about a subset and DeviceInfo doesn't mark the rest as optional)
+
+    def press(self) -> None:
+        """Toggles the movement of the corresponding shutter"""
+        self._stick.command(self._duofernId, "toggle")
