521a13c0ef0fb7e016e815012f2823c290fb6eeb
gluap <github@pgoergen.de>
2023-03-11 12:13:26 +0100

    Add sun sensor support as contributed by @lrunsdorf in #29 (with a few modifications to adapt it to the new device support)

--- custom_components/duofern/binary_sensor.py
+++ custom_components/duofern/binary_sensor.py
@@ -47,6 +47,10 @@ async def async_setup_entry(
                 continue
 
             async_add_entities([DuofernSmokeDetector(device['id'], device['name'], stick, hass)]) # Add new binary sensor for smoke detectors
+        if device['id'].startswith('a5'): # Check if this device is a sun sensor
+            if device['id'] in hass.data[DOMAIN]['devices'].keys(): # Check if Home Assistant already has this device
+                continue
+            async_add_entities([DuofernSunSensor(device['id'], device['name'], stick, hass)]) # Add new binary sensor for sun sensors
 
 
 class DuofernSmokeDetector(BinarySensorEntity):
@@ -118,3 +122,68 @@ class DuofernSmokeDetector(BinarySensorEntity):
             self._battery_level = self._stick.duofern_parser.get_state(self._code, 'batteryLevel', channel=self._channel)
         except KeyError:
             self._battery_level = None
+
+class DuofernSunSensor(BinarySensorEntity):
+    """Duofern sun sensor entity"""
+
+    def __init__(self, code, desc, stick, hass, channel=None):
+        """Initialize the sun sensor"""
+
+        self._code = code
+        self._id = code
+        self._name = desc
+
+        if channel:
+          chanNo = "{:02x}".format(channel)
+          self._id += chanNo
+          self._name += chanNo
+
+        self._state: str | None = None # Holds the state (off = clear, on = smoke detected)
+        self._stick = stick # Hold an instance of the Duofern stick
+        self._channel = channel
+        hass.data[DOMAIN]['devices'][self._id] = self # Add device to our domain
+
+    @property
+    def name(self):
+        """Returns the name of the sun sensor"""
+        return self._name
+
+    @property
+    def is_on(self):
+        """Returns the current state of the sun sensor"""
+        return self._state == "on"
+
+    @property
+    def device_state_attributes(self):
+        """Return the attributes of the sun sensor"""
+        attributes = {
+        }
+
+        return attributes
+    @property
+    def icon(self):
+        """Return the icon of the sun sensor"""
+        return "mdi:sun-wireless-outline"
+
+    @property
+    def device_class(self):
+        """Return the device class sun sensor"""
+        return BinarySensorDeviceClass.LIGHT
+
+    @property
+    def should_poll(self):
+        """Whether this entity should be polled or uses subscriptions"""
+        return True # TODO: Add config option for subscriptions over polling
+
+    @property
+    def unique_id(self):
+        """Return the unique id of the Duofern device"""
+        return self._id
+
+    def update(self):
+        """Called right before is_on() to update the current state from the stick"""
+        try:
+            self._state = self._stick.duofern_parser.get_state(self._code, 'state', channel=self._channel)
+        except KeyError:
+            self._state = None
+
